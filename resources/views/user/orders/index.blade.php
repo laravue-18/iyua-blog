5ccb49ecb370bea2cc33cb5d70cd59a3522434a8
laravue-18 <yurijklimovv@gmail.com>
2021-11-05 19:53:47 +0800

    commit

--- resources/views/user/orders/index.blade.php
+++ resources/views/user/orders/index.blade.php
@@ -15,12 +15,6 @@
                 <table class="table table-centered table-nowrap">
                     <thead class="thead-light">
                         <tr>
-                            <th style="width: 20px;">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                    <label class="custom-control-label" for="customCheck1">&nbsp;</label>
-                                </div>
-                            </th>
                             <th>OrderID</th>
                             <th>Buyer</th>
                             <th>Product</th>
@@ -28,19 +22,12 @@
                             <th>Shipping Address</th>
                             <th>Amount</th>
                             <th>Date</th>
-                            <th>Action</th>
+{{--                            <th>Action</th>--}}
                         </tr>
                     </thead>
                     <tbody>
                         @foreach($orders as $order)
                             <tr>
-                                <td>
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck2">
-                                        <label class="custom-control-label" for="customCheck2">&nbsp;</label>
-                                    </div>
-                                </td>
-
                                 <td>{{ $order->order_id }}</td>
                                 <td>{{ $order->user->name }}</td>
                                 <td>{{ $order->product ? $order->product->name : 'Deleted' }}</td>
@@ -48,10 +35,10 @@
                                 <td>{{ $order->shipping_address }}</td>
                                 <td>$ {{ $order->amount }}</td>
                                 <td>{{ $order->created_at }}</td>
-                                <td>
-                                    <a href="javascript:void(0);" class="mr-3 text-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="mdi mdi-pencil font-size-18"></i></a>
-                                    <a href="javascript:void(0);" class="text-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="mdi mdi-close font-size-18"></i></a>
-                                </td>
+{{--                                <td>--}}
+{{--                                    <a href="javascript:void(0);" class="mr-3 text-primary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><i class="mdi mdi-pencil font-size-18"></i></a>--}}
+{{--                                    <a href="javascript:void(0);" class="text-danger" data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete"><i class="mdi mdi-close font-size-18"></i></a>--}}
+{{--                                </td>--}}
                             </tr>
                         @endforeach
 
